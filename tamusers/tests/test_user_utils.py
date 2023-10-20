e65610cfb4a64be1961577518f2660f2831b903a
Dan Jacob <dan.jacob@anders.com>
2023-10-20 10:38:43 +0300

    Handle duplicate or empty email addresses
    When creating a new account, check if email already exists. We should
    also avoid creating EmailAddress instance if the provider does not
    include an email.
    
    In cases where the payload is missing email, do not try to create an
    EmailAddress instance. If another user already has this email address,
    we should also not try and create a new EmailAddress instance.
    
    In addition, if there is an existing SocialAccount matching another user,
    raise an Authentication error instead of doing an assert.
    
    Refs: TTVA-173

--- tamusers/tests/test_user_utils.py
+++ tamusers/tests/test_user_utils.py
@@ -2,6 +2,8 @@ import uuid
 
 import pytest
 from allauth.socialaccount.models import EmailAddress, SocialAccount
+from django.test import override_settings
+from rest_framework import exceptions
 
 from tamusers.user_utils import get_or_create_user
 
@@ -14,7 +16,14 @@ class TestGetOrCreateUser:
             "email": "tester@gmail.com",
         }
 
-    @pytest.mark.django_db()
+    @pytest.mark.django_db(transaction=True)
+    def test_new_user_payload_missing_sub(self, user_model):
+        with pytest.raises(exceptions.AuthenticationFailed):
+            get_or_create_user({}, oidc=False)
+
+        assert user_model.objects.count() == 0
+
+    @pytest.mark.django_db(transaction=True)
     def test_new_user(self, payload):
         user = get_or_create_user(payload, oidc=False)
         assert user.email == "tester@gmail.com"
@@ -30,7 +39,35 @@ class TestGetOrCreateUser:
         assert address.user == user
         assert address.email == "tester@gmail.com"
 
-    @pytest.mark.django_db()
+    @pytest.mark.django_db(transaction=True)
+    def test_new_user_not_using_social_account(self, payload, settings):
+        installed_apps = settings.INSTALLED_APPS[::]
+        installed_apps.remove("allauth.socialaccount")
+
+        with override_settings(INSTALLED_APPS=installed_apps):
+            user = get_or_create_user(payload, oidc=False)
+            assert user.email == "tester@gmail.com"
+
+        assert SocialAccount.objects.count() == 0
+        assert EmailAddress.objects.count() == 0
+
+    @pytest.mark.django_db(transaction=True)
+    def test_new_user_oidc(self, payload):
+        user = get_or_create_user(payload, oidc=True)
+        assert user.email == "tester@gmail.com"
+
+        account = SocialAccount.objects.get()
+
+        assert account.user == user
+        assert account.extra_data == payload
+        assert account.provider == "tampere_oidc"
+        assert account.uid == payload["sub"]
+
+        address = EmailAddress.objects.get()
+        assert address.user == user
+        assert address.email == "tester@gmail.com"
+
+    @pytest.mark.django_db(transaction=True)
     def test_new_user_no_email(self, payload):
         del payload["email"]
         user = get_or_create_user(payload, oidc=False)
@@ -46,7 +83,29 @@ class TestGetOrCreateUser:
         # No email address created
         assert EmailAddress.objects.count() == 0
 
-    @pytest.mark.django_db()
+    @pytest.mark.django_db(transaction=True)
+    def test_new_user_no_email_existing_empty_email(self, user_model, payload):
+        del payload["email"]
+
+        address = EmailAddress.objects.create(
+            user=user_model.objects.create_user(username="tester"), email=""
+        )
+
+        user = get_or_create_user(payload, oidc=False)
+        assert user.email == ""
+
+        account = SocialAccount.objects.get()
+
+        assert account.user == user
+        assert account.extra_data == payload
+        assert account.provider == "tampere"
+        assert account.uid == payload["sub"]
+
+        # No email address created
+        assert EmailAddress.objects.count() == 1
+        assert EmailAddress.objects.first().user == address.user
+
+    @pytest.mark.django_db(transaction=True)
     def test_existing_user(self, user_model, payload):
         existing_user = user_model.objects.create(
             email="tester@gmail.com", uuid=payload["sub"]
@@ -68,7 +127,27 @@ class TestGetOrCreateUser:
         assert address.user == user
         assert address.email == "tester@gmail.com"
 
-    @pytest.mark.django_db()
+    @pytest.mark.django_db(transaction=True)
+    def test_existing_user_empty_email(self, user_model, payload):
+        del payload["email"]
+
+        existing_user = user_model.objects.create(email="", uuid=payload["sub"])
+
+        user = get_or_create_user(payload, oidc=False)
+
+        assert user.uuid.hex == existing_user.uuid
+        assert user.email == existing_user.email
+
+        account = SocialAccount.objects.get()
+
+        assert account.user == user
+        assert account.extra_data == payload
+        assert account.provider == "tampere"
+        assert account.uid == payload["sub"]
+
+        assert EmailAddress.objects.count() == 0
+
+    @pytest.mark.django_db(transaction=True)
     def test_existing_social_account(self, user_model, payload):
         existing_user = user_model.objects.create(
             email="tester@gmail.com", uuid=payload["sub"]
@@ -95,15 +174,15 @@ class TestGetOrCreateUser:
         # No email address created
         assert EmailAddress.objects.count() == 0
 
-    @pytest.mark.django_db()
+    @pytest.mark.django_db(transaction=True)
     def test_existing_email(self, user_model, payload):
         existing_user = user_model.objects.create(
             email="tester@gmail.com", uuid=payload["sub"]
         )
 
-        user = get_or_create_user(payload, oidc=False)
+        EmailAddress.objects.create(user=existing_user, email=existing_user.email)
 
-        EmailAddress.objects.create(user=user, email=existing_user.email)
+        user = get_or_create_user(payload, oidc=False)
 
         assert user.uuid.hex == existing_user.uuid
         assert user.email == existing_user.email
@@ -118,3 +197,38 @@ class TestGetOrCreateUser:
         address = EmailAddress.objects.get()
         assert address.user == user
         assert address.email == "tester@gmail.com"
+
+    @pytest.mark.django_db(transaction=True)
+    def test_existing_email_for_other_user(self, user_model, payload):
+        existing_user = user_model.objects.create(
+            email="tester-2@gmail.com", uuid=uuid.uuid4().hex
+        )
+
+        EmailAddress.objects.create(user=existing_user, email="tester@gmail.com")
+
+        get_or_create_user(payload, oidc=False)
+
+        assert user_model.objects.count() == 2
+        assert SocialAccount.objects.count() == 1
+        assert EmailAddress.objects.count() == 1
+
+        assert EmailAddress.objects.first().user == existing_user
+
+    @pytest.mark.django_db(transaction=True)
+    def test_existing_social_account_for_other_user(self, user_model, payload):
+        existing_user = user_model.objects.create(
+            email="tester-2@gmail.com", uuid=uuid.uuid4().hex
+        )
+
+        SocialAccount.objects.create(
+            user=existing_user,
+            uid=payload["sub"],
+            provider="tampere",
+        )
+
+        with pytest.raises(exceptions.AuthenticationFailed):
+            get_or_create_user(payload, oidc=False)
+
+        assert user_model.objects.count() == 1
+        assert SocialAccount.objects.count() == 1
+        assert EmailAddress.objects.count() == 0
--- tamusers/user_utils.py
+++ tamusers/user_utils.py
@@ -1,8 +1,25 @@
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django.db import transaction
 from django.utils.translation import ugettext as _
 from rest_framework import exceptions
+
+try:
+    from allauth.socialaccount.models import EmailAddress, SocialAccount
+
+    is_allauth = True
+except ImportError:
+    is_allauth = False
+
+
+# TBD: these should be in Django settings
+PROVIDER = "tampere"
+OIDC_PROVIDER = "tampere_oidc"
+
+
+def use_social_account():
+    return is_allauth and "allauth.socialaccount" in settings.INSTALLED_APPS
 
 
 def oidc_to_user_data(payload):
@@ -56,6 +73,7 @@ def update_user(user, payload, oidc=False):
         user.update_ad_groups(ad_groups)
 
 
+@transaction.atomic()
 def get_or_create_user(payload, oidc=False):
     user_id = payload.get("sub")
     if not user_id:
@@ -64,41 +82,81 @@ def get_or_create_user(payload, oidc=False):
 
     user_model = get_user_model()
 
-    with transaction.atomic():
-        try:
-            user = user_model.objects.select_for_update().get(uuid=user_id)
-        except user_model.DoesNotExist:
-            user = user_model(uuid=user_id)
-            user.set_unusable_password()
-        update_user(user, payload, oidc)
+    try:
+        user = user_model.objects.select_for_update().get(uuid=user_id)
+    except user_model.DoesNotExist:
+        user = user_model(uuid=user_id)
+        user.set_unusable_password()
 
-    # If allauth.socialaccount is installed, create the SocialAcount
-    # that corresponds to this user. Otherwise logins through
-    # allauth will not work for the user later on.
-    if "allauth.socialaccount" in settings.INSTALLED_APPS:
-        from allauth.socialaccount.models import EmailAddress, SocialAccount
-
-        if oidc:
-            provider_name = "tampere_oidc"
-        else:
-            provider_name = "tampere"
-        args = {"provider": provider_name, "uid": user_id}
-        try:
-            account = SocialAccount.objects.get(**args)
-            assert account.user_id == user.id
-        except SocialAccount.DoesNotExist:
-            account = SocialAccount(**args)
-            account.extra_data = payload
-            account.user = user
-            account.save()
-
-            try:
-                email = EmailAddress.objects.get(email__iexact=user.email)
-                assert email.user == user
-            except EmailAddress.DoesNotExist:
-                email = EmailAddress(
-                    email=user.email.lower(), primary=True, user=user, verified=True
-                )
-                email.save()
+    update_user(user, payload, oidc)
+    create_social_account(user, user_id, payload, oidc)
 
     return user
+
+
+def create_social_account(user, user_id, payload, oidc):
+    """Creates SocialAccount instance for this user if one
+    does not exist already.
+
+    Raises AuthenticationFailed if existing SocialAccount matching provider
+    and UUID for another user.
+
+    If user does not have an existing SocialAccount, will also attempt to create or match an
+    EmailAddress instance.
+    """
+    if not use_social_account():
+        return None
+
+    provider_name = OIDC_PROVIDER if oidc else PROVIDER
+    kwargs = {"provider": provider_name, "uid": user_id}
+
+    try:
+        account = SocialAccount.objects.get(**kwargs)
+
+        if account.user_id != user.pk:
+            msg = _("User already exists for this account.")
+            raise exceptions.AuthenticationFailed(msg)
+
+    except SocialAccount.DoesNotExist:
+        account = SocialAccount.objects.create(
+            **kwargs,
+            user=user,
+            extra_data=payload,
+        )
+        create_email_address(user)
+
+    return account
+
+
+def create_email_address(user):
+    """Creates EmailAddress instance for this user if no address
+    can be found.
+
+
+    Returns None if user does not have an email address, or if the email address matching
+    that of the user already exists for another user.
+    """
+    if not use_social_account():
+        return None
+
+    email = (user.email or "").strip().lower()
+
+    if not email:
+        return None
+
+    try:
+        # ensure this email address does not already exist
+        address = EmailAddress.objects.get(email__iexact=email)
+
+        if address.user_id != user.pk:
+            return None
+
+    except EmailAddress.DoesNotExist:
+        address = EmailAddress.objects.create(
+            user=user,
+            email=email,
+            primary=True,
+            verified=True,
+        )
+
+    return address
